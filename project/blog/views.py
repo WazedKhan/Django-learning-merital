aeb23b74e5ac1ed235b40817959f3d4d47a92ad8
WazedKhan <wazedkhan119399@gmail.com>
2024-07-08 00:58:34 +0600

    MVT circle

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse("<H1>Hello!</H1>")
+
+
+def about(request):
+    return HttpResponse("<H1>About Me!</H1>")
